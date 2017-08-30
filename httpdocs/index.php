51a37186843136e4c34f4d14d76cb9668507cbaf
Aphichat Panjamanee <panjamanee@gmail.com>
2017-08-30 16:24:40 +0200

    Changed from .php to .html

--- httpdocs/index.php
+++
@@ -1,5 +0,0 @@
-<h1>Hello World!</h1>
-<p>This is the project of MFE with custom public folder. This file sits nicely in httpdocs.</p>
-<i>v0.2</i>
-
-<img src="https://images.unsplash.com/photo-1478028928718-7bfdb1b32095?dpr=1&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop=" alt="">
